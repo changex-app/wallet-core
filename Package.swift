b03e050fc68431f1353527adaadbcd46c847d084
hewigovens <360470+hewigovens@users.noreply.github.com>
2022-04-07 19:31:08 +0900

    bump master SPM build version (#2129)

--- Package.swift
+++ Package.swift
@@ -12,13 +12,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WalletCore",
-            url: "https://github.com/ValeriD/wallet-core-1/releases/download/2.7.2/WalletCore.xcframework.zip",
-            checksum: "3b8b12adc5658f93e8bbc043a06629c7c8047dadbf95a7125226ca68f8898b30"
+            url: "https://github.com/trustwallet/wallet-core/releases/download/2.7.7/WalletCore.xcframework.zip",
+            checksum: "6326e5c44405d7fe7c639fbbf00006be13b1db177adf678b1dba43cf6c8cf7a3"
         ),
         .binaryTarget(
             name: "SwiftProtobuf",
-            url: "https://github.com/ValeriD/wallet-core-1/releases/download/2.7.2/SwiftProtobuf.xcframework.zip",
-            checksum: "4c95eb3d53b237afa93ae0a97071fdd7da2ea773db668093a97170cc6c42b024"
+            url: "https://github.com/trustwallet/wallet-core/releases/download/2.7.7/SwiftProtobuf.xcframework.zip",
+            checksum: "27e6020522c7d2ebd221680929bac1f09c2027cbecaec449aef3912e440f41bd"
         )
     ]
 )
